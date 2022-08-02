716d34a3644fe54bea5c8cea1a6e7c098195b25d
LiamOSDev :) <110433916+LiamOSDevDeveloper@users.noreply.github.com>
2022-08-01 19:29:34 -0500

    Add files via upload

---
+++ LiamOS Update Calculator-SourceCode/LiamOS Update Calculator/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace LiamOS_Update_Calculator
+{
+    partial class StartScreen
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartScreen));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.VersionName = new System.Windows.Forms.TextBox();
+            this.ContinueBTN = new System.Windows.Forms.Button();
+            this.ExitBTN = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(414, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(584, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LiamOS Release Update Calculator";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox2.Image = global::LiamOS_Update_Calculator.Properties.Resources.LiamOS;
+            this.pictureBox2.Location = new System.Drawing.Point(651, 84);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(104, 100);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(435, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Version Name:";
+            // 
+            // VersionName
+            // 
+            this.VersionName.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VersionName.Location = new System.Drawing.Point(584, 373);
+            this.VersionName.Name = "VersionName";
+            this.VersionName.Size = new System.Drawing.Size(223, 33);
+            this.VersionName.TabIndex = 4;
+            this.VersionName.TextChanged += new System.EventHandler(this.VersionName_TextChanged);
+            // 
+            // ContinueBTN
+            // 
+            this.ContinueBTN.BackColor = System.Drawing.Color.Lime;
+            this.ContinueBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ContinueBTN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ContinueBTN.Location = new System.Drawing.Point(651, 412);
+            this.ContinueBTN.Name = "ContinueBTN";
+            this.ContinueBTN.Size = new System.Drawing.Size(75, 52);
+            this.ContinueBTN.TabIndex = 5;
+            this.ContinueBTN.Text = "Continue";
+            this.ContinueBTN.UseVisualStyleBackColor = false;
+            // 
+            // ExitBTN
+            // 
+            this.ExitBTN.BackColor = System.Drawing.Color.Red;
+            this.ExitBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitBTN.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitBTN.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ExitBTN.Location = new System.Drawing.Point(1278, 516);
+            this.ExitBTN.Name = "ExitBTN";
+            this.ExitBTN.Size = new System.Drawing.Size(189, 106);
+            this.ExitBTN.TabIndex = 6;
+            this.ExitBTN.Text = "EXIT";
+            this.ExitBTN.UseVisualStyleBackColor = false;
+            this.ExitBTN.Click += new System.EventHandler(this.ExitBTN_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1525, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(995, 352);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(150, 100);
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // StartScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1525, 634);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.ExitBTN);
+            this.Controls.Add(this.ContinueBTN);
+            this.Controls.Add(this.VersionName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "StartScreen";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LiamOS Update Calculator V1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox VersionName;
+        private System.Windows.Forms.Button ContinueBTN;
+        private System.Windows.Forms.Button ExitBTN;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ LiamOS Update Calculator-SourceCode/LiamOS Update Calculator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiamOS_Update_Calculator
+{
+    public partial class StartScreen : Form
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ContinueBTN.Enabled = false;
+        }
+
+        private void ExitBTN_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+        private void Controlbuttons()
+        {
+            if (VersionName.Text.Trim() != string.Empty && VersionName.Text.All(Char.IsLetter))
+            {
+                ContinueBTN.Enabled = true;
+                errorProvider1.SetError(VersionName, "");
+            }
+            else
+            {
+                if (!(VersionName.Text.All(Char.IsLetter)))
+                {
+                    errorProvider1.SetError(VersionName, "The Version Only Contains Letters.");
+                }
+                else
+                {
+                    errorProvider1.SetError(VersionName, "Put Any Version Name Example: LiamOS , LiamOSsHell");
+                }
+                ContinueBTN.Enabled = false;
+                VersionName.Focus();
+            }
+        }
+        private void VersionName_TextChanged(object sender, EventArgs e)
+        {
+            Controlbuttons();
+        }
+    }
+}
